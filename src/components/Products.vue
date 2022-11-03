0052be17e267d60f03ca2d2a3e1788342e664a9e
tutran0904 <tutran0904@gmail.com>
2022-11-03 09:31:33 +0700

    Done click in datatable

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -3,7 +3,8 @@
 	<div>
         <div class="card">
             <h5>Products</h5>
-            <DataTable :value="customers2" :scrollable="true" scrollHeight="600px" :loading="loading" scrollDirection="both">
+            <DataTable :value="customers2" v-model:selection="selectedProducts3" :scrollable="true" scrollHeight="500px" :loading="loading" scrollDirection="both" dataKey="id"  @rowSelect="onRowSelect"  @rowUnselect="onRowUnselect">
+                <Column selectionMode="multiple" headerStyle="width: 3em"></Column>
                 <Column field="id" header="Id" footer="Id" style="width:100px"></Column>
                 <Column field="title" header="title" footer="title" style="width:200px"></Column>
                 <Column field="brand" header="brand" footer="brand" style="width:200px"></Column>
@@ -23,9 +24,22 @@
 
     const customers2 = ref();
     const loading = ref(false);
+    const selectedProducts3 = ref();
 
     const productStore = useProductStore();
     // const { products } = storeToRefs(productStore);
+
+    const getOneData = () => {
+        console.log("Hello World");
+    }
+
+    const onRowSelect = (event) => {
+        console.log("Chon data", event.data);
+    }
+
+    const onRowUnselect = (event) => { 
+        console.log("Bo chon data", event.data);
+    }
 
     onMounted(() => {
         loading.value = true;
@@ -34,6 +48,9 @@
             loading.value = false;
         });
     })
+
+   
+
 
 </script>
 
